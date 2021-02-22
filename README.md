64abc41a4763deba0c358fe7a0afa0b4cfd9d5b6
LouisBillaut <louisbillaut2000@gmail.com>
2021-02-22 18:55:22 +0100

    initial version commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# IGM Autoreact V2 bot
+This bot allows to react automatically on the "emargement" channel of IGM server.
+This is against Discord TOS, to use at your own risks.
+
+## DOWNLOADS
+Install Nodejs: https://nodejs.org/en/download/  
+⚠️ Warning: discord.js v.11 is required, v.12 not support user tokens.  
+To install discord.js v.11: `npm install discord.js@11`
+
+## CONFIGURATION
+config.json is the configuration file of the bot.  
+You must write on the first line, your discord token.  
+⚠️ Warning: your discord token is a private information ! You must never make it public  
+Nice video which explain how to get it: https://www.youtube.com/watch?v=YEgFvgg7ZPI
+
+## RUN
+`node autoclickerV2.js`
+And let the bot run !
---
+++ autoclickerV2.js
@@ -0,0 +1,24 @@
+//Warning: discord.js v.11 is required, v.12 not support user tokens.
+//To install discord.js v.11: npm install discord.js@11
+
+//main func
+const Discord = require("discord.js");
+const user = new Discord.Client();
+const config = require("./config.json");
+//Emargement channel from IGM Discord
+let channel = "773863921206558761";
+let usertoken = config.token;
+user.login(usertoken);
+user.on("ready", ready => {
+  user.on("message", message => {
+    if (channel.includes(message.channel.id) ) {
+      message.embeds.forEach(m => {
+        //test if this is the bot "appel" message
+        if(m.title.toString().includes("Appel demandé")){
+          message.react("🙋");
+          console.log("reaction added !")
+        }
+      })
+    }
+  });
+});
